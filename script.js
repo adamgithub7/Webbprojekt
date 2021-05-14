3e79ee7cb5867666645444755ffe46a53c007210
adamgithub7 <adam.almir@edu.jonkoping.sem>
2021-05-14 16:28:37 +0200

    Update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,11 +15,13 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
@@ -25,18 +29,4 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-var images = new Array();
 
-function preloadImages(){
-
-    for (i=0; i < preloadImages.arguments.length; i++){
-
-         images[i] = new Image();
-
-        images[i].src = preloadImages.arguments[i];
-
-    }
-
-}
-
-preloadImages("slideimg1.jpg");
